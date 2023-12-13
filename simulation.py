9d54559e9f6a302537d2976e39247f998d7dd192
nickmvega <nvega@uchicago.edu>
2023-12-13 13:15:12 -0500

    Implmentation

--- simulation.py
+++ simulation.py
@@ -4,45 +4,69 @@ from GSPC_model import process_data_and_backtest
 import yfinance as yf
 
 def simulate_trading_algorithm(starting_balance, data, index_date):
-    # Initialize variables
     current_balance = starting_balance
     shares_held = 0
     cost_basis = 0
 
-    for i, current_date in enumerate(index_date[:-1]): 
-        # Extract relevant data
+    trading_log = []
+
+    for i, current_date in enumerate(index_date[:-1]):
+
         row = data.loc[current_date]
         prediction = row['Predictions_SP500']
         vfiax_close = row['VFIAX_close']
 
-        # Buy decision
+        # Buy
         if prediction == 1 and shares_held == 0:
             shares_bought = current_balance / vfiax_close
             cost_basis = current_balance
             shares_held += shares_bought
             current_balance = 0
-            print("------")
-            print(f"Date: {current_date}\nDecision: Buy {shares_bought} shares at {vfiax_close} per share.\nCurrent balance: {current_balance}\n")
-            print("------")
 
-        # Hold decision
+            trading_log.append({
+                'Date': current_date,
+                'Prediction': prediction,
+                'Decision': 'Buy',
+                'Shares': shares_bought,
+                'Price': vfiax_close,
+                'Profit/Loss': 0,
+                'Current Balance': current_balance
+            })
+
+        # Hold
         elif prediction == 1 and shares_held > 0:
             current_balance = shares_held * vfiax_close
-            print("------")
-            print(f"Date: {current_date}\nDecision: Hold.\nCurrent balance: {current_balance}\n")
-            print("------")
 
-        # Sell decision
+            trading_log.append({
+                'Date': current_date,
+                'Prediction': prediction,
+                'Decision': 'Hold',
+                'Shares': shares_held,
+                'Price': vfiax_close,
+                'Profit/Loss': 0,
+                'Current Balance': current_balance
+            })
+
+        # Sell
         elif prediction == 0 and shares_held > 0:
             current_balance = shares_held * vfiax_close
             profit_loss = current_balance - cost_basis
             shares_held = 0
             cost_basis = 0
-            print("------")
-            print(f"Date: {current_date}\nDecision:Sell at {vfiax_close} per share.\nProfit/Loss: {profit_loss}.\nCurrent balance: {current_balance}\n")
-            print("------")
 
-    return current_balance
+            trading_log.append({
+                'Date': current_date,
+                'Prediction': prediction,
+                'Decision': 'Sell',
+                'Shares': 0,
+                'Price': vfiax_close,
+                'Profit/Loss': profit_loss,
+                'Current Balance': current_balance
+            })
+
+    trading_log_df = pd.concat([pd.DataFrame([x]).round({'Shares': 2, 'Price': 2, 'Profit/Loss': 2, 'Current Balance': 2}) for x in trading_log], ignore_index=True)
+
+    return current_balance, trading_log_df
 
 if __name__ == "__main__":
     vfiax = yf.Ticker("VFIAX")
@@ -52,7 +76,9 @@ if __name__ == "__main__":
 
     sp500 = pd.merge(sp500, vfiax['Close'].rename('VFIAX_close'), left_index=True, right_index=True, how='left')
 
-    result = simulate_trading_algorithm(10000, sp500, sp500.index)
+    result, trading_log = simulate_trading_algorithm(10000, sp500, sp500.index)
 
     print("Final balance:", result)
 
+    print("\nTrading Log:")
+    print(trading_log)
